ce5e56246bff4cb7657aac49a7ff8f267897b0df
guill <guill@LAPTOP-8368E17A>
2017-04-18 18:41:45 -0700

    Add prefabs and scripts to pick the wave user wants to play.

---
+++ SpaceShoot1/Assets/VRButton.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRStandardAssets.Utils;
+
+public class VRButton : MonoBehaviour {
+
+    [SerializeField] private Button m_Button;
+    [SerializeField] private VRInteractiveItem m_InteractiveItem;
+    [SerializeField] private VRInput m_VRInput;
+    [SerializeField] private AudioSource m_Audio;                       // Reference to the audio source that will play effects when the user looks at it and when it fills.
+    [SerializeField] private AudioClip m_OnOverClip;                    // The clip to play when the user looks at the button.
+    [SerializeField] private AudioClip m_OnPressedClip;                  // The clip to play when the button is pressed.
+
+    private bool m_Pressed = false;
+    private bool m_GazeOver = false;                                            // Whether the user is currently looking at the bar.
+
+    public VRInput VRInput
+    {
+        get
+        {
+            return m_VRInput;
+        }
+
+        set
+        {
+            m_VRInput = value;
+
+            m_VRInput.OnDown += HandleDown;
+            m_VRInput.OnUp += HandleUp;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (VRInput)
+        {
+            VRInput.OnDown += HandleDown;
+            VRInput.OnUp += HandleUp;
+        }
+
+        m_InteractiveItem.OnOver += HandleOver;
+        m_InteractiveItem.OnOut += HandleOut;
+    }
+
+    private void HandleOut()
+    {
+        m_GazeOver = false;
+
+        if (m_Pressed)
+            return;
+
+        m_Button.image.color = m_Button.colors.normalColor;
+    }
+
+    private void HandleOver()
+    {
+        m_GazeOver = true;
+
+        if (m_Pressed)
+            return;
+
+        m_Button.image.color = m_Button.colors.highlightedColor;
+
+        m_Audio.clip = m_OnOverClip;
+        m_Audio.Play();
+    }
+
+    private void HandleUp()
+    {
+        // TODO: handle up
+    }
+
+    private void HandleDown()
+    {
+        if (m_GazeOver)
+        {
+            m_Button.image.color = m_Button.colors.pressedColor;
+            m_Pressed = true;
+
+            m_Audio.clip = m_OnPressedClip;
+            m_Audio.Play();
+
+            // TODO: add an action that triggers when user select the button
+        }
+    }
+
+    private void OnDisable()
+    {
+        VRInput.OnDown -= HandleDown;
+        VRInput.OnUp -= HandleUp;
+
+        m_InteractiveItem.OnOver -= HandleOver;
+        m_InteractiveItem.OnOut -= HandleOut;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SpaceShoot1/Assets/WaveSelectionController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRStandardAssets.Utils;
+
+public class WaveSelectionController : MonoBehaviour {
+
+    [SerializeField] private Canvas m_UICanvas;
+    [SerializeField] private GameObject m_VRButonPrefab;
+    [SerializeField] private VRInput m_VRInput;
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            var newButton = Instantiate(m_VRButonPrefab);
+            newButton.GetComponent<VRButton>().VRInput = m_VRInput;
+
+            newButton.transform.SetParent(m_UICanvas.transform);
+
+            var btn = newButton.GetComponent<Button>();
+            var txt = btn.GetComponentInChildren<Text>();
+            txt.text = (i + 1).ToString();
+            var rectTransform = newButton.transform as RectTransform;
+            var width = rectTransform.rect.width;
+            rectTransform.localPosition = new Vector3(i * width, 0, 0);
+            rectTransform.localScale = Vector3.one;
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
